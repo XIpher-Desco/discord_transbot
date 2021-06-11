a59149a5c1717a374b901a342cde1964c91a6e0d
XIpher-Desco <yukino.konata@gmail.com>
2021-06-11 19:20:43 +0900

    全角ｍも除外対応

--- main.py
+++ main.py
@@ -218,6 +218,8 @@ async def on_message(message):
     # 一部特定文字の場合即時リターン (先頭! とか / のやつ)
     if "m" == trancslate_text:
         return
+    if "ｍ" == trancslate_text:
+        return
     elif re.match(r"/xien", trancslate_text):
         # 日本語 -> 英語コマンドの判定
         ja_to_en = True
